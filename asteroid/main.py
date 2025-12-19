6189fa7cb94718db5d3d8d87ed16183cc1419209
Andrei <krestonosa@gmail.com>
2025-12-19 21:05:57 +0000

    starting new project

---
+++ asteroid/main.py
@@ -0,0 +1,17 @@
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
+
+VERSION = pygame.version.ver
+
+pygame.init()
+    
+
+
+def main():
+    print(f"Starting Asteroids with pygame version: {VERSION}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+if __name__ == "__main__":
+    main()
