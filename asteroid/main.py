1b84bd46cd85c04a0ac36c7c452dc317f5ce78f7
Andrei <krestonosa@gmail.com>
2026-02-02 19:13:40 +0100

    adding visuals for player

--- asteroid/main.py
+++ asteroid/main.py
@@ -1,18 +1,22 @@
 import pygame
+
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while 1:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         print(dt)
