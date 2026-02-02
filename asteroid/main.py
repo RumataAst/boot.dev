8f87a853af8707fa7946f08b6d0c6309e93076b2
Andrei <krestonosa@gmail.com>
2026-02-02 20:15:07 +0100

    learning about groups for classes and pygame

--- asteroid/main.py
+++ asteroid/main.py
@@ -8,15 +8,26 @@ def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
+
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     while 1:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return 
         screen.fill("black")
-        player.draw(screen)
+        updatable.update(dt)
+
+        for screen_object in drawable:
+            screen_object.draw(screen)
+        
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         print(dt)
